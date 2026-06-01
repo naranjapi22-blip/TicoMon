9ff88eed025f920fff1bb04089828c02fed8b57d
DarOrange <naranjapi22@gmai.com>
2026-06-01 17:42:27 -0600

    Añadido gitignore para ignorar archivos sensibles

--- # Crea un archivo .py
+++
@@ -1,5 +0,0 @@
-# Crea un archivo .env en la misma carpeta donde ejecutes este script
-with open(".env", "w") as f:
-    f.write("TOKEN=TU_TOKEN_AQUI_PEGADO")
-    
-print("Archivo .env creado con éxito.")
