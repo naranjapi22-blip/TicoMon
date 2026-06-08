47604c88a64385db85e05d3cfef9062c8b758396
DarOrange <naranjapi22@gmail.com>
2026-06-08 09:28:40 -0600

    Catch Rate 1.1

--- vistas.py
+++ vistas.py
@@ -359,8 +359,8 @@ class BotonCaptura(discord.ui.View):
             # --- MATEMÁTICA DE CAPTURA ---
             azar = random.random()
             if azar < 0.005: bonus_bola, nombre_bola = 255.0, "Master Ball"
-            elif azar < 0.15: bonus_bola, nombre_bola = 2.0, "Ultra Ball"
-            elif azar < 0.40: bonus_bola, nombre_bola = 1.5, "Great Ball"
+            elif azar < 0.15: bonus_bola, nombre_bola = 1.5, "Ultra Ball"
+            elif azar < 0.40: bonus_bola, nombre_bola = 1.25, "Great Ball"
             else: bonus_bola, nombre_bola = 1.0, "Pokéball"
 
             multiplicador_shiny = 1.0 # Ajustado para que los shiny sean atrapables
