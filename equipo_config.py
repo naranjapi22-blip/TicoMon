063a8b4898e7a2497bba99d98f17eb7075611515
DarOrange <naranjapi22@gmai.com>
2026-06-15 21:23:08 -0600

    Soccer 1.17

--- equipo_config.py
+++ equipo_config.py
@@ -1,15 +1,7 @@
-def equipo_a_dict(equipo):
-
-    return {
-        "portero": equipo[1],
-        "defensa_1": equipo[2],
-        "defensa_2": equipo[3],
-        "defensa_3": equipo[4],
-        "defensa_4": equipo[5],
-        "medio_1": equipo[6],
-        "medio_2": equipo[7],
-        "medio_3": equipo[8],
-        "medio_4": equipo[9],
-        "delantero_1": equipo[10],
-        "delantero_2": equipo[11],
-    }
+# formación 4-4-2 fija
+POSICIONES_FUTBOL = [
+    "portero",
+    "defensa_1", "defensa_2", "defensa_3", "defensa_4",
+    "medio_1", "medio_2", "medio_3", "medio_4",
+    "delantero_1", "delantero_2"
+]
