b5a787b7f919e3a2a111f57a670a93ec91dd598d
DarOrange <naranjapi22@gmai.com>
2026-06-16 00:12:57 -0600

    Soccer 1.46

--- simulador_futbol.py
+++ simulador_futbol.py
@@ -1,20 +1,6 @@
 import random
 from futbol import ordenar_equipo_por_formacion
-def equipo_a_dict(equipo):
 
-    return {
-        "portero": equipo[1],
-        "defensa_1": equipo[2],
-        "defensa_2": equipo[3],
-        "defensa_3": equipo[4],
-        "defensa_4": equipo[5],
-        "medio_1": equipo[6],
-        "medio_2": equipo[7],
-        "medio_3": equipo[8],
-        "medio_4": equipo[9],
-        "delantero_1": equipo[10],
-        "delantero_2": equipo[11],
-    }
 def formatear_timeline(resultado, nombre_a, nombre_b):
 
     eventos = sorted(resultado["eventos"], key=lambda e: e["minuto"])
