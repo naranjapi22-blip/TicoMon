ea6dfef048c1ad3a6ec1d6c56aac8877e3099ecd
DarOrange <naranjapi22@gmai.com>
2026-06-19 00:57:13 -0600

    Trainers 1.16

--- vistas.py
+++ vistas.py
@@ -690,6 +690,7 @@ class BotonCaptura(discord.ui.View):
                                 )
                             ],
                             view=None
+                        )
 
                         self.stop()
 
