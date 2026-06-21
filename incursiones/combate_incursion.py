68fb7f05f89e590accdc38cd069c3c6e22d23f08
DarOrange <naranjapi22@gmai.com>
2026-06-21 16:19:19 -0600

    Incursion 5.6

--- incursiones/combate_incursion.py
+++ incursiones/combate_incursion.py
@@ -1,33 +1,11 @@
 from combate_servicios import preparar_equipo_desde_capturas
 from incursiones.incursion_manager import eliminar_incursion
 from incursiones.vista_combate_incursion import VistaCombateIncursion
+from incursiones.alpha_factory import crear_alpha
 
-
-alpha = [{
-    "nombre": "Alpha Mewtwo",
-    "species_showdown": "mewtwo",
-    "nature_showdown": "modest",
-    "tipo": ["psychic"],
-    "ivs": {
-        "hp": 31,
-        "atk": 31,
-        "def": 31,
-        "spa": 31,
-        "spd": 31,
-        "spe": 31
-    },
-    "atk": 220,
-    "atk_esp": 350,
-    "def": 220,
-    "def_esp": 220,
-    "spd": 180,
-    "hp_max": 30000,
-    "movimiento": "psystrike",
-    "movimiento_nombre": "Psystrike",
-    "id": 150,
-    "shiny": False
-}]
-
+alpha = crear_alpha(
+    "mewtwo"
+)
 
 async def iniciar_incursion(
     raid,
