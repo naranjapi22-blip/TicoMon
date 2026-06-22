deeb6f167e0287dea073b4d18356d37030bf5af1
DarOrange <naranjapi22@gmai.com>
2026-06-21 19:06:14 -0600

    Incursion 5.7

--- incursiones/combate_incursion.py
+++ incursiones/combate_incursion.py
@@ -4,7 +4,7 @@ from incursiones.vista_combate_incursion import VistaCombateIncursion
 from incursiones.alpha_factory import crear_alpha
 
 alpha = crear_alpha(
-    "mewtwo"
+    "Mewtwo"
 )
 
 async def iniciar_incursion(
