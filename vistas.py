bd2f72dab2234c2df2f3542c470b0d27c6bd2bb0
DarOrange <naranjapi22@gmai.com>
2026-06-26 20:55:13 -0600

    Captura 1.7

--- vistas.py
+++ vistas.py
@@ -671,20 +671,22 @@ class BotonCaptura(discord.ui.View):
                                 "Has entrado en el Salón de la Fama."
                             )
 
-                            try:
-                                await interaction.message.delete()
-                            except discord.NotFound:
-                                pass
+                        # ← Aquí termina el if/elif
 
-                            await interaction.channel.send(
-                                content=mensaje,
-                                file=discord.File(
-                                    buffer_captura,
-                                    filename="captura.gif"
-                                )
+                        try:
+                            await interaction.message.delete()
+                        except discord.NotFound:
+                            pass
+
+                        await interaction.channel.send(
+                            content=mensaje,
+                            file=discord.File(
+                                buffer_captura,
+                                filename="captura.gif"
                             )
+                        )
 
-                            self.stop()
+                        self.stop()
 
                     except Exception as db_error:
 
